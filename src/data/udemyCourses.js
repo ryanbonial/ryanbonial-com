c6be7de2668b66c3d93784aa08f152bbb1a5e420
Ryan Bonial <ryanbonial@gmail.com>
2020-04-28 14:44:28 -0600

    Load udemy course images locally

--- src/data/udemyCourses.js
+++ src/data/udemyCourses.js
@@ -3,187 +3,187 @@ const UdemyCourses = [
     title: 'AWS Serverless APIs & Apps',
     completedDate: 'April 2020',
     certificateUrl: 'https://www.udemy.com/certificate/UC-e43d2c67-d0b7-4ffb-8ec6-6fde5d2fb3ff/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1241254_9cc1.jpg'
+    imageUrl: '/images/udemy-courses/1241254_9cc1.jpg'
   },
   {
     title: 'Learn how to DJ From The Wu-Tang Clan',
     completedDate: 'April 2020',
     certificateUrl: 'https://www.udemy.com/certificate/UC-40f061ae-6296-4225-bc59-01e13c530761/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1186942_5deb.jpg'
+    imageUrl: '/images/udemy-courses/1186942_5deb.jpg'
   },
   {
     title: 'The Ultimate MySQL Bootcamp',
     completedDate: 'April 2020',
     certificateUrl: 'https://www.udemy.com/certificate/UC-790ffdd2-6a85-4159-a880-3ba3997ec3f4',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1187016_51b3.jpg'
+    imageUrl: '/images/udemy-courses/1187016_51b3.jpg'
   },
   {
     title: 'Practical Ethical Hacking',
     completedDate: 'Feb 2020',
     certificateUrl: 'https://www.udemy.com/certificate/UC-587ea4ad-f982-4c97-804c-888b05d5c396',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/2642432_ac8f_3.jpg'
+    imageUrl: '/images/udemy-courses/2642432_ac8f_3.jpg'
   },
   {
     title: 'Hands-on Penetration Testing Labs 2.0',
     completedDate: 'Dec 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-OSBK6LXF',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/2133014_9810_7.jpg'
+    imageUrl: '/images/udemy-courses/2133014_9810_7.jpg'
   },
   {
     title: 'Ethical Hacking',
     completedDate: 'Oct 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-0YD1Z6SP',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/437490_c76a_4.jpg'
+    imageUrl: '/images/udemy-courses/437490_c76a_4.jpg'
   },
   {
     title: 'Intro to Ethical Hacking',
     completedDate: 'Sep 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-EQ55NGCK/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1035598_bec4_3.jpg'
+    imageUrl: '/images/udemy-courses/1035598_bec4_3.jpg'
   },
   {
     title: 'Deep Learning Prerequisites',
     completedDate: 'Jul 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-XOSFI99A',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/980086_196d_2.jpg'
+    imageUrl: '/images/udemy-courses/980086_196d_2.jpg'
   },
   {
     title: 'Svelte.js',
     completedDate: 'Jul 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-RJS3JD1M',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/2360566_d008.jpg'
+    imageUrl: '/images/udemy-courses/2360566_d008.jpg'
   },
   {
     title: 'Deep Learning Computer Vision',
     completedDate: 'Jun 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-K6Q01TT9',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1930180_27d3_6.jpg'
+    imageUrl: '/images/udemy-courses/1930180_27d3_6.jpg'
   },
   {
     title: 'OrientDB',
     completedDate: 'May 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-TJP8YBLG',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/308072_edc7_4.jpg'
+    imageUrl: '/images/udemy-courses/308072_edc7_4.jpg'
   },
   {
     title: 'Advanced React and Redux',
     completedDate: 'Feb 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-19CDB6FE',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/781532_8b4d_6.jpg'
+    imageUrl: '/images/udemy-courses/781532_8b4d_6.jpg'
   },
   {
     title: 'GraphQL with React',
     completedDate: 'Jan 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-JHRECHR9',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1109926_7f97_2.jpg'
+    imageUrl: '/images/udemy-courses/1109926_7f97_2.jpg'
   },
   {
     title: 'Modern React with Redux',
     completedDate: 'Jan 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-4G32CLG8',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/705264_caa9_11.jpg'
+    imageUrl: '/images/udemy-courses/705264_caa9_11.jpg'
   },
   {
     title: 'Android Oreo',
     completedDate: 'Feb 2019',
     certificateUrl: 'https://www.udemy.com/certificate/UC-JHRECHR9/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1405812_931d_2.jpg'
+    imageUrl: '/images/udemy-courses/1405812_931d_2.jpg'
   },
   {
     title: 'Angular with Angular Material, Angularfire & NgRx',
     completedDate: 'Feb 2018',
     certificateUrl: 'https://www.udemy.com/certificate/UC-47BKFCRC',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1512962_9f57.jpg'
+    imageUrl: '/images/udemy-courses/1512962_9f57.jpg'
   },
   {
     title: 'Electron for Desktop Apps',
     completedDate: 'Feb 2018',
     certificateUrl: 'https://www.udemy.com/certificate/UC-YCY32DQJ/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1200530_21e0_5.jpg'
+    imageUrl: '/images/udemy-courses/1200530_21e0_5.jpg'
   },
   {
     title: 'Phaser 2.x',
     completedDate: 'Jan 2018',
     certificateUrl: 'https://www.udemy.com/certificate/UC-OCTAU0PJ/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/710488_fb11.jpg'
+    imageUrl: '/images/udemy-courses/710488_fb11.jpg'
   },
   {
     title: 'React 16',
     completedDate: 'Jan 2018',
     certificateUrl: 'https://www.udemy.com/certificate/UC-I60DZB06',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1362070_b9a1_2.jpg'
+    imageUrl: '/images/udemy-courses/1362070_b9a1_2.jpg'
   },
   {
     title: 'Webpack 2',
     completedDate: 'Oct 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-MWIOY9H4',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1039062_e553_4.jpg'
+    imageUrl: '/images/udemy-courses/1039062_e553_4.jpg'
   },
   {
     title: 'Go: The Complete Developer\'s Guide (Golang)',
     completedDate: 'Sep 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-LTXWC14P',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1309202_1231_4.jpg'
+    imageUrl: '/images/udemy-courses/1309202_1231_4.jpg'
   },
   {
     title: 'MEAN Stack',
     completedDate: 'Sep 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-FJ6F4AZ2',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/833442_b26e_4.jpg'
+    imageUrl: '/images/udemy-courses/833442_b26e_4.jpg'
   },
   {
     title: 'The Complete Developers Guide to MongoDB',
     completedDate: 'Sep 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-O2AEO4GL',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1000574_06cb.jpg'
+    imageUrl: '/images/udemy-courses/1000574_06cb.jpg'
   },
   {
     title: 'Vue JS 2',
     completedDate: 'Sep 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-AAMPJREQ',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/995016_ebf4.jpg'
+    imageUrl: '/images/udemy-courses/995016_ebf4.jpg'
   },
   {
     title: 'Angular Styling & Animations',
     completedDate: 'Aug 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-JDZ4VFY3/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1145430_68e5.jpg'
+    imageUrl: '/images/udemy-courses/1145430_68e5.jpg'
   },
   {
     title: 'ES6 JavaScript',
     completedDate: 'Jul 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-N1F9G8DE/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/886614_68cc.jpg'
+    imageUrl: '/images/udemy-courses/886614_68cc.jpg'
   },
   {
     title: 'Angular 4',
     completedDate: 'Jun 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-9YTOL4LV/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/756150_c033_2.jpg'
+    imageUrl: '/images/udemy-courses/756150_c033_2.jpg'
   },
   {
     title: 'Ionic 2/3',
     completedDate: 'Jun 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-KPJLJ4N2/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1070124_3a0f_4.jpg'
+    imageUrl: '/images/udemy-courses/1070124_3a0f_4.jpg'
   },
   {
     title: 'Docker Mastery',
     completedDate: 'Apr 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-HKB6B7UD/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/1035000_c1aa_6.jpg'
+    imageUrl: '/images/udemy-courses/1035000_c1aa_6.jpg'
   },
   {
     title: 'Git Complete',
     completedDate: 'Apr 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-F77Q370N/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/221674_1411_4.jpg'
+    imageUrl: '/images/udemy-courses/221674_1411_4.jpg'
   },
   {
     title: 'Understanding Typescript',
     completedDate: 'Apr 2017',
     certificateUrl: 'https://www.udemy.com/certificate/UC-7JX4ZZ89/',
-    imageUrl: 'https://img-a.udemycdn.com/course/240x135/947098_02ec.jpg'
+    imageUrl: '/images/udemy-courses/947098_02ec.jpg'
   },
 
 ];
